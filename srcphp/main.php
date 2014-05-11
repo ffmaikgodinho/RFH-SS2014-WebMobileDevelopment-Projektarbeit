304be531b2e9678fee8e211eff9af2cecc56b6a6
Denis Kündgen <d.kuendgen@netcologne.de>
2014-05-11 16:40:55 +0200

    CSS
    - Testseite main.php erstellt
    - CSS-Datei geschrieben
    - Testlogo eingefügt

---
+++ srcphp/main.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/desktop.css">
+		<title>Mainpage</title>
+	</head>
+	<body>
+		<div id="share">
+		Facebook | Google | Twitter
+		</div>
+		<div id="navigation" >
+			<span id="logo">
+				<a href=""><img src="img/logo.gif" alt="logo"></a>
+			</span>
+			<span id="navigation_buttons">
+				<a href="">About</a>
+				<a href="">New</a> 
+				<a href="">Login</a>
+			</span>
+		</div>
+		<div id="content">
+			<p id="location">
+				Home > test
+			</p>
+			content<br>
+			content2<br>
+			content<br>
+			content2<br>
+			content<br>
+			content2<br>
+			content<br>
+			content2<br>
+			content<br>
+			content2<br>
+			content<br>
+			content2<br>
+		</div>
+	</body>
+</html>
