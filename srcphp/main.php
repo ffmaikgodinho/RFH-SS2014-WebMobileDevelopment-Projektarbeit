b5039bdfdf4b0bb2eff2a8dea17ffa7ea726b5ce
Denis Kündgen <d.kuendgen@netcologne.de>
2014-05-12 01:03:57 +0200

    New Design

--- srcphp/main.php
+++ srcphp/main.php
@@ -8,34 +8,123 @@
 	</head>
 	<body>
 		<div id="share">
-		Facebook | Google | Twitter
+			<p id="share_content">
+				Facebook | Google | Twitter
+			</p>
 		</div>
-		<div id="navigation" >
-			<span id="logo">
-				<a href=""><img src="img/logo.gif" alt="logo"></a>
-			</span>
-			<span id="navigation_buttons">
+		<div id="logo_area">
+			<a href=""><img src="img/logo.gif" alt="logo" id="logo"></a>
+		</div>
+		<div id="navigation">
+			<p id="navigation_buttons">
 				<a href="">About</a>
 				<a href="">New</a> 
 				<a href="">Login</a>
-			</span>
+			</p>
 		</div>
-		<div id="content">
-			<p id="location">
+		<div id="location">
+			<p id="location_content">
 				Home > test
 			</p>
-			content<br>
-			content2<br>
-			content<br>
-			content2<br>
-			content<br>
-			content2<br>
-			content<br>
-			content2<br>
-			content<br>
-			content2<br>
-			content<br>
-			content2<br>
+		</div>
+		<div id="menue" align="right">
+			<p id="menue_gradient"></p>
+			<a href=""><p class="menue_button" valign="middle">Button 1</p></a>
+			<p class="menue_button">Button 2</p>
+			<p class="menue_button">Button 3</p>
+		</div>
+		<div id="content">
+			<p id="content_content">
+				blacontent<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+				content<br>
+				content2<br>
+			</p>
+		</div>
+		<div id="footer">
+			<p id="copyright">
+				copyright 2014 bla bla blub | Impressum
+			</p>
 		</div>
 	</body>
 </html>
