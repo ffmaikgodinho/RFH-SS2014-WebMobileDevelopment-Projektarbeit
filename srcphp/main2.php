c408b55336ea851a55cf3b0569ac4fef26f43a1b
Denis Kündgen <d.kuendgen@netcologne.de>
2014-05-31 14:22:15 +0200

    work on Stylesheets

--- srcphp/main2.php
+++ srcphp/main2.php
@@ -7,111 +7,190 @@
 		<title>Mainpage</title>
 	</head>
 	<body>
-		<h1 id="logo">
-			DIE SEITE
-		</h1>
-		<menu id="navigation">
-			<a href=""><li id="navigation">About</li></a>
-			<a href=""><li id="navigation">New</li></a>
-			<a href=""><li id="navigation">Login</li></a>
-		</menu>
-		<!--<a href=""><img src="img/logo.gif" alt="logo" id="logo"></a>-->
-		<h2 id="position">
-			About
-		</h2>
-		<div id="content">
-			<p id="content_content">
-				blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-				content<br>
-				content2<br>
-			</p>
+		<div id="outer-header">
+			<div id="inner-header">
+				<h1 id="logo">
+					pick-it!
+				</h1>
+				<menu id="navigation">
+					<a href=""><li id="navigation">About</li></a>
+					<a href=""><li id="navigation">New</li></a>
+					<a href=""><li id="navigation">Login</li></a>
+				</menu>
+				<form action="test.php" id="search" method="get">
+					<input class="search" type="search" lang="de" name="search" placeholder="Search" maxlength="30"></>
+					<input class="search" type="image" src="img/search.png" alt="Suche">
+				</form>
+			</div>
 		</div>
+		<!--<a href=""><img src="img/logo.gif" alt="logo" id="logo"></a>-->
+		<div id="content-limiter">
+			<div class="content-item">
+				<div class="content-head">
+					<p class="content-title">
+						Einladung zur Grillparty
+					</p>
+					<p class="content-creator">
+						erstellt von d3nis
+					</p>
+				</div>
+				<div class="content-area">
+					blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+				</div>
+			</div>
+			<div class="content-item">
+				<div id="content-head">
+					<p class="content-title">
+						Einladung zur Grillparty
+					</p>
+					<p class="content-creator">
+						erstellt von d3nis
+					</p>
+				</div>
+				<div class="content-area">
+					blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+				</div>
+			</div><div class="content-item">
+				<div id="content-head">
+					<p class="content-title">
+						Einladung zur Grillparty
+					</p>
+					<p class="content-creator">
+						erstellt von d3nis
+					</p>
+				</div>
+				<div class="content-area">
+					blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+				</div>
+			</div><div class="content-item">
+				<div id="content-head">
+					<p class="content-title">
+						Einladung zur Grillparty
+					</p>
+					<p class="content-creator">
+						erstellt von d3nis
+					</p>
+				</div>
+				<div class="content-area">
+					blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+				</div>
+			</div><div class="content-item">
+				<div id="content-head">
+					<p class="content-title">
+						Einladung zur Grillparty
+					</p>
+					<p class="content-creator">
+						erstellt von d3nis
+					</p>
+				</div>
+				<div class="content-area">
+					blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+				</div>
+			</div><div class="content-item">
+				<div id="content-head">
+					<p class="content-title">
+						Einladung zur Grillparty
+					</p>
+					<p class="content-creator">
+						erstellt von d3nis
+					</p>
+				</div>
+				<div class="content-area">
+					blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+				</div>
+			</div><div class="content-item">
+				<div id="content-head">
+					<p class="content-title">
+						Einladung zur Grillparty
+					</p>
+					<p class="content-creator">
+						erstellt von d3nis
+					</p>
+				</div>
+				<div class="content-area">
+					blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+				</div>
+			</div><div class="content-item">
+				<div id="content-head">
+					<p class="content-title">
+						Einladung zur Grillparty
+					</p>
+					<p class="content-creator">
+						erstellt von d3nis
+					</p>
+				</div>
+				<div class="content-area">
+					blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+				</div>
+			</div><div class="content-item">
+				<div id="content-head">
+					<p class="content-title">
+						Einladung zur Grillparty
+					</p>
+					<p class="content-creator">
+						erstellt von d3nis
+					</p>
+				</div>
+				<div class="content-area">
+					blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent blacontent<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+					content2<br>
+					content<br>
+				</div>
+			</div>
 		<div id="footer">
 			copyright 2014 bla bla blub | Impressum
-		</div>
-		<div id="share">
-			Facebook | Google | Twitter
 		</div>
 	</body>
 </html>
