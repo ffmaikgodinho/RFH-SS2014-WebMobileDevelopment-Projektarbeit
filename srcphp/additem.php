3e512e7c99daaae1792e6a7b7f20a7f97b946702
nzieg <github@project89.com>
2014-06-14 14:38:29 +0200

    inline updating an item

--- srcphp/additem.php
+++ srcphp/additem.php
@@ -2,69 +2,50 @@
 <html>
 	<head>
 		<title>Eintrag anlegen</title>
-		<!--<link rel="stylesheet" type="text/css" href="style.css">-->
+		<style type="text/css">
+			.entry {}
+			.entry-title { color: #44cccc; margin: 5px; }
+			.entry-qty {margin: 5px;}
+			.entry-note {margin: 5px;}
+		</style>
+		<script type="text/javascript">
+			function showNewEntryForm() {
+				document.getElementById('newEntry').style.visibility = "visible";
+			}
+		</script>
 	</head>
 	<body> 
 		<?php 	
-			$form_title = "Eintrag erstellen";
-			
 			if(isset($_GET["eventid"])){
 				$q_eventid = $_GET["eventid"];
 			} else {
 				$q_eventid = 0;
 			}
 			
-			// hidden form field if on edit mode
-			$q_editmode = FALSE;
+			// get entryid
+			if(isset($_GET["id"])){
+				$currentID = $_GET["id"];
+			} else {
+				$currentID = 0;
+			}
 			
-			// variables for form fields
-			$q_title = "";
-			$q_note = "";
-			$q_link = "";
-			$q_total_qty = "";
-			
-			// Read SQL Params if inUrl "?mode=edit"
-			if(isset($_GET["mode"]) AND $_GET["mode"]=="edit") {
-			
-				// header on editmode
-				$form_title = "Eintrag bearbeiten";
+			// Initialize form params
+			if(isset($_GET["mode"])) {
 				
-				// get entryid
-				if(isset($_GET["id"])){
-					$currentID = $_GET["id"];
-				} else {
-					$currentID = 0;
-				}
-				
-				if(isset($_GET["eventid"])){
-					$eventid = $_GET["eventid"];
-				} else {
-					$eventid = 0;
-				}
-				
-				$query = "SELECT id, eventid, title, note, link, total_qty 
+				$query = "SELECT id, eventid, title, note, total_qty 
 						  FROM entry 
 						  WHERE id=$currentID";
 						  
 				//mysql_query($query);
-				
-				//debug  
-				echo $query;
-				
-				// variables for form fields
-				$q_id = $currentID;
-				$q_title = "Title"; // ToDo: Get from query
-				$q_note = "Notiz"; // ToDo: Get from query
-				$q_link = "http://google.de"; // ToDo: Get from query
-				$q_total_qty = "10"; // ToDo: Get from query
-				$q_eventid = $eventid; // ToDo: Get from query
-				$q_editmode = TRUE;
 			}
 			
 			// Write input to database on submit
 			 if(isset($_POST['submit'])) {
 			  
-			 	$id = $_POST["id"];
+				if (isset($_POST["entryid"])) {
+					$id = $_POST["entryid"];
+				}
+				
 				$eventid = $_POST["eventid"];
 				$title = $_POST["title"];
 				$total_qty = $_POST["total_qty"];
@@ -75,22 +56,16 @@
 					$note = $_POST["note"];
 				}
 				
-				if (empty($_POST["link"])){
-					$link = "";
-				} else {
-					$link = $_POST["link"];
-				}
-				
-				if($_POST["editmode"] == TRUE) {
+				if($_POST["mode"] == "update") {
 				$query = "UPDATE entry 
 						  SET eventid='$eventid', 
+							  title='$title',
 						      note='$note', 
-						      link = '$link', 
 						      total_qty = '$total_qty'
 							WHERE id=$id";
-				} else {
-					$query = "INSERT INTO entry (eventid, note, link, total_qty)
-									VALUES ('$eventid', '$note', '$link', '$total_qty')";
+				} else if ($_POST["mode"] == "new") {
+					$query = "INSERT INTO entry (eventid, title, note, total_qty)
+									VALUES ('$eventid', '$title', '$note', '$total_qty')";
 				}
 
 				//debug
@@ -98,41 +73,63 @@
 				//mysql_query($query);
 			 }
 
-
+			
 		?>
 
-		<form action="additem.php" method="post" class="form-container">
-			<div class="form-title">
-				<h2><?php echo $form_title; ?></h2>
-			</div>
-			<div class="form-title">
-				Titel*: 
-			</div> 
-			<input type="text" name="title" class="form-field" required="required" value="<?php echo $q_title; ?>" />
-			<br />
-			<div class="form-title">
-				Menge: 
-			</div> 
-			<input type="number" name="total_qty" min="0" value="<?php echo $q_total_qty; ?>" />
-			<br />
-			<div class="form-title">
-				Link: 
-			</div> 
-			<input type="url" name="link" value="<?php echo $q_link; ?>" />
-			<br />
-			<div class="form-title">
-				Notiz: 
-			</div> 
-			<textarea rows="4" cols="50" name="note" class="form-field"><?php echo $q_note; ?></textarea>
-			<br />
+		<!-- Show all Items -->
+		<?php 
+			// ToDo: $entries = get_allentrys()
+			$entries = [[1, 1, "Brot", 2, ""],[2, 1, "Salat", 4, "Notiz"]];
+			
+			$userLoggedIn = TRUE;
+			if($userLoggedIn) { 
+				$disabled = "";
+			} else {
+				$disabled = "disabled";
+			}
+			
+			foreach ($entries as list($entry_id, $event_id, $entry_title, $entry_qty, $entry_note)) {
+				echo "<form action=\"additem.php\" method=\"post\" id=\"editEntry\" class=\"form-container\">";
+				echo "<input type=\"text\" name=\"title\" class=\"form-field\" required=\"required\" value=\"".$entry_title."\" ".$disabled." />";
+				echo "<input type=\"number\" name=\"total_qty\" min=\"0\" value=\"".$entry_qty."\" ".$disabled." />";
+				echo "<textarea rows=\"2\" cols=\"50\" name=\"note\" class=\"form-field\" ".$disabled.">".$entry_note."</textarea>";
+				
+				echo "<input type=\"hidden\" name=\"eventid\" value=\"".$event_id."\"/>";
+				echo "<input type=\"hidden\" name=\"entryid\" value=\"".$entry_id."\"/>";
+				echo "<input type=\"hidden\" name=\"mode\" value=\"update\"/>";
+				
+				if ($userLoggedIn) {
+					echo "<input type=\"submit\" name=\"submit\" class=\"submit-button\" value=\"Speichern\">";
+				}
+				echo "</form>";
+			}
+		?>
+		
+		<!-- New Item -->
+		<?php
+		
+		$userIsAdmin = $userLoggedIn;
+		if ($userIsAdmin) {
+			echo "<img src=\"img\\new-icon.png\" onclick=\"javascript:showNewEntryForm()\">";
+		}
+		?>
+		
+		<div class="content-item" id="newEntry" style="visibility:hidden">
+			<form action="additem.php" method="post" class="form-container">
+				<div class="form-title">
+					<h2>Neuer Eintrag</h2>
+				</div>
+				<input type="text" name="title" class="form-field" required="required" value="" />
+				<input type="number" name="total_qty" min="0" value="" />
+				<textarea rows="4" cols="50" name="note" class="form-field"></textarea>
 
-			<input type="hidden" name="id" value="<?php echo $q_id; ?>" />
-			<input type="hidden" name="eventid" value="<?php echo $q_eventid; ?>" />
-			<input type="hidden" name="editmode" value="<?php echo $q_editmode; ?>"/>
-			<div class="submit-container">
-				<input type="submit" name="submit" class="submit-button" value="Speichern">
-			</div>
-		</form>
-	</body>
+				<input type="hidden" name="id" value="" />
+				<input type="hidden" name="eventid" value="" />
+				<input type="hidden" name="mode" value="new"/>
+				<div class="submit-container">
+					<input type="submit" name="submit" class="submit-button" value="Speichern">
+				</div>
+			</form>
+		</div>
 	</body>
 </html>
