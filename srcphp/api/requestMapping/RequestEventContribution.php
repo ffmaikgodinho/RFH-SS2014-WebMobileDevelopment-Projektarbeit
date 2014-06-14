429769b77d8ee891bacb72b76188d6f5b4e118ee
ffmaikgodinho <maik.godinho@flowfact.de>
2014-06-14 12:15:24 +0200

    RequestHandler EventContribution completed

--- srcphp/api/requestMapping/RequestEventContribution.php
+++ srcphp/api/requestMapping/RequestEventContribution.php
@@ -64,7 +64,21 @@
          * @return if successfull returns the inserted id
          */
         public function create($inputData)  {
-            $this->m_requestHandler->responseNotImplemented("Not Yet implemented");
+            if ($this->checkData($inputData))  {
+                $strSql =   "INSERT INTO `contribution` (`userid`, `name`, `entryid`, `quantity`, `stamp`) ".
+                        "VALUES ('".$inputData->userid."', '".$inputData->name."', '".$inputData->eventEntryID."', '".$inputData->quantity."', '1');";
+                $result = $this->m_requestHandler->getDatabase()->query($strSql);
+                $lastid = $this->m_requestHandler->getDatabase()->getLastInsertID();
+                if (is_int($lastid) && $lastid > 0)  {
+                    return $lastid;
+                }
+                else  {
+                    $this->m_requestHandler->responseInternalServerError("EventContribution could not be inserted.");
+                }                
+            }
+            else {
+                $this->m_requestHandler->responseBadRequest("Not all needed EventContribution information have been send.");
+            }
         }
         
         /**
@@ -76,7 +90,21 @@
          * @return void
          */
         public function update($inputData)  {
-            $this->m_requestHandler->responseNotImplemented("Not Yet implemented");
+            //check wethere the user gave us a correct version (the latest)
+            $event = $this->getSingle(($inputData->id));
+            if ($inputData->stamp == $event->stamp)  {
+                $strSql = "UPDATE contribution SET name = '".$inputData->name."',quantity = '".$inputData->quantity."', stamp = stamp + 1 WHERE id = '" . $inputData->id . "'";
+                $result = $this->m_requestHandler->getDatabase()->query($strSql);
+                if ($this->m_requestHandler->getDatabase()->getAffectedRows() != 1)  {
+                    $this->m_requestHandler->responseNotFound("The given id was not found and therefore could not be updated..");
+                }
+                else  {
+                    $this->m_requestHandler->responseOK("EventContribution successfully updated.");
+                }                
+            }
+            else  {
+                $this->m_requestHandler->responsePreconditionFailes("Given version is outdated.");
+            }
         }
         
         /**
@@ -97,6 +125,27 @@
                 $this->m_requestHandler->responseOK("Contribution successfully deleted.");
             }
         }
+        
+        
+        /**
+         * RequestEventEntry::checkData()
+         *
+         * checks wethere the required fields are filled to insert data 
+         *  
+         * @param mixed $data
+         * @return boolean | True if all nesseccessary data are filled
+         */
+        private function checkData($data)  {
+            if (is_numeric($data->eventEntryID))  {
+                if (is_numeric($data->quantity))  {
+                    if (strlen($data->name) > 0)  {
+                        return true;   
+                    }
+                }
+            }
+            return false;
+        }
+        
     }
 
 ?>
